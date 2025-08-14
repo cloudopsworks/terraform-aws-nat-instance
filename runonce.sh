1972388b3a5731a91a42bee99c14863d1122a617
Cristian Beraha <cristian@cloudops.works>
2025-08-14 10:23:19 -0300

    Update network interface references and metadata paths in scripts for compatibility with new ENI setup.

--- runonce.sh
+++ runonce.sh
@@ -2,8 +2,8 @@
 
 # attach the ENI
 aws ec2 attach-network-interface \
-  --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
-  --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)" \
+  --region "$(/usr/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
+  --instance-id "$(/usr/bin/ec2-metadata -i | cut -d' ' -f2)" \
   --device-index 1 \
   --network-interface-id "${eni_id}"
 
